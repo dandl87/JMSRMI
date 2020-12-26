a1c6804259965f2de8c38bae67ab7acb6d75fcda
hp <hp@hp-Daniele>
2020-12-26 15:50:58 +0100

    Nuova funzionalità string

--- RealServices/src/SimpleApp/MainSimple.java
+++ RealServices/src/SimpleApp/MainSimple.java
@@ -9,16 +9,30 @@ public class MainSimple {
 		System.out.println("App di prova configuration managment ");
 		Scanner sc = new Scanner(System.in);
 		while(true) {
+			System.out.println("OP:1 quadrato - 2 string composition");
+			int op = sc.nextInt();
+			if(op==1) {
 		System.out.println("inserisci un intero");
 		try {
 			int x =sc.nextInt(); 
-		System.out.println("il quadrato di "+x+" è "+x*x);
+		System.out.println("il quadrato di "+x+" ï¿½ "+x*x);
 	}catch(InputMismatchException e) {
 		System.out.println("lanciata eccezione:"+e); 
 		System.out.println("End of Program");
 		break;
 	} 
-		
+			}else if(op==2) {
+				System.out.println("Inserisci una stringa");
+				String x = sc.next();
+				StringBuffer a = new StringBuffer("la stringa :");
+				a.append(x);
+				x= new String(a);
+				System.out.println(x);
+				
+			}else {
+				System.out.println("exit");
+				break;
+			}
 	 
 		}
 		sc.close();
